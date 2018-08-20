337220d87097d3d4165335d440b84ae0caec03e0
Caibiy <caibiy666@gmail.com>
2018-08-20 18:12:54 +0800

    a

---
+++ night-test/test.js
@@ -0,0 +1,24 @@
+/*
+* @Author: Caibiy
+* @Date:   2018-08-20 17:29:49
+* @Last Modified by:   Caibiy
+* @Last Modified time: 2018-08-20 18:10:15
+*/
+const Nightmare = require('nightmare')
+const nightmare = Nightmare({ show: true,dock:false })
+
+nightmare
+  .goto('https://www.baidu.com')
+  .click('#u1 > a.lb')
+  .wait('#TANGRAM__PSP_10__footerULoginBtn')
+  .click('#TANGRAM__PSP_10__footerULoginBtn')
+  .type('#TANGRAM__PSP_10__userName','17326191153')
+  .type('#TANGRAM__PSP_10__password','jiaDONG1234&')
+  .click('#TANGRAM__PSP_10__submit')
+  .wait('#s_menu_mine > div.mine-text')
+  .evaluate(() => document.querySelector('#s_menu_mine > div.mine-text').innerHTML)
+  .end()
+  .then(console.log)
+  .catch(error => {
+    console.error('Search failed:', error)
+  })
