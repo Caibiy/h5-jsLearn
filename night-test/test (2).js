7d04426093fea8451b1f4254553772428d1d921d
Caibiy <caibiy666@gmail.com>
2018-08-22 17:34:43 +0800

    a

--- night-test/test (2).js
+++ night-test/test (2).js
@@ -20,7 +20,7 @@ nightmare
 })*/
 // Second instance:
 const Nightmare = require('nightmare')
-const  arr = ['http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5'];
+const  arr = ['http://tieba.baidu.com/f?kw=%BA%BC%D6%DD&fr=ala0&tpl=5'];
 	(function(){
 			var nightmare2 = Nightmare({show: true})
 		  nightmare2.goto('https://www.baidu.com')
@@ -28,7 +28,7 @@ const  arr = ['http://tieba.baidu.com/f?kw=%BD%E4%C9%AB&fr=ala0&tpl=5'];
 		  .wait('#TANGRAM__PSP_10__footerULoginBtn')
 		  .click('#TANGRAM__PSP_10__footerULoginBtn')
 		  .wait(2000)
-		  .type('#TANGRAM__PSP_10__userName','279285839@qq.com')
+		  .type('#TANGRAM__PSP_10__userName','17326191153')
 		  .type('#TANGRAM__PSP_10__password','jiaDONG1234&')
 		  .click('#TANGRAM__PSP_10__submit')
 		  .wait(10000)
@@ -44,17 +44,30 @@ index++;
 nightmare2
   .goto(arr[index%arr.length])
   .wait('a.j_th_tit ')
-  .evaluate(()=>{return document.querySelectorAll("a.j_th_tit ")[Math.ceil((Math.random()*100)%4)+4].href})
+  .evaluate(()=>{return document.querySelectorAll("a.j_th_tit ")[Math.ceil((Math.random()*100)%4)+5].href})
   .then((href=>{
 	  console.log(href);
 	  nightmare2.goto(href)
 	  //core_title_txt
+	  /*
+	  .click(document.querySelector('.lzl_link_unfold :nth-child(1)'))
+	  .wait(".j_editor_for_container")
+	  .type(".j_editor_for_container","希望可以帮助到迷茫的人http://www.9ilu.com/74.html")
+	  .click(".lzl_panel_submit")
+	  */
 	  .wait("h1.core_title_txt")
-	  .type("#ueditor_replace","希望可以帮助到迷茫的人http://www.9ilu.com/74.html")
-	  .click(".ui_btn_m")
-	  .wait(15000)
+	  .wait(10000)
 	  .evaluate(()=>{
-		  return document.querySelector('h1.core_title_txt').innerText;
+		  /*
+		  $('.lzl_link_unfold :nth-child(1)').click();
+		  $(".j_editor_for_container").text("http://www.9ilu.com/74.html");
+		  $('.lzl_panel_submit').click();*/
+		  var arr = $(".lzl_link_unfold");
+		  for(var i=0;i<arr.length;i++){if(arr[i].innerText=="回复"){$(arr[i]).click();$(arr[i]).parent().parent().parent().find("#j_editor_for_container").text("aaa");$(arr[i]).parent().parent().parent().find(".lzl_panel_wrapper .lzl_panel_submit.j_lzl_p_sb").click();break;}}
+	  })
+	  .wait(1000)
+	  .evaluate(()=>{
+		   return document.querySelector('h1.core_title_txt').innerText;
 	  })
 	  .then((text)=>{
 			 console.log(text);
